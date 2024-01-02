ffc6231b5f2500c29b337d0e198bc76e93cce204
Lilroy007 <antonio.bertinau@gmail.com>
2024-01-02 03:42:34 +0100

    test function

---
+++ scripts/config.js
@@ -0,0 +1,3 @@
+let listeMots = ["Cachalot", "Pétunia", "Serviette"]
+let listePhrases = ["Le cachalot est un mammifère marin", "Le pétunia est une fleur", "La serviette est un objet du quotidien"]
+
