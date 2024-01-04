eb70bd94e68890af8ed2dbc007bce1051b40e2b4
Lilroy007 <antonio.bertinau@gmail.com>
2024-01-04 18:54:46 +0100

    test InnerHTML

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,16 @@
 lancerJeu()
 
-let zoneSaisie = document.getElementById(inputEcriture)
-console.log(zoneSaisie)
+//let inputEcriture = document.getElementById("inputEcriture")
+//console.log(inputEcriture)
+
+//let btnValiderMot = document.getElementById("btnValiderMot")
+//console.log(btnValiderMot)
+
+//let zoneProposition = document.querySelector(".zoneProposition")
+//console.log(zoneProposition)
+
+//let spanScore = document.querySelector(".zoneScore span")
+//console.log(spanScore)
+
+//let listeBtnRadio = document.querySelectorAll(".optionSource input")
+//console.log(listeBtnRadio)
